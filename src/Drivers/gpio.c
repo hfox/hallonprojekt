becc83b2e6dd3d13c1bf337cced2c814cd518d44
Simon Larsson <snigelson@gmail.com>
2013-07-17 17:27:22 +0200

    [Drivers] Added more registers and functions to enable/disable edge detection to the UART driver, while keeping coding style

--- src/Drivers/gpio.c
+++ src/Drivers/gpio.c
@@ -3,6 +3,8 @@
  *
  **/
 
+#include "gpio.h"
+
 typedef struct {
 	unsigned long	GPFSEL[6];	///< Function selection registers.
 	unsigned long	Reserved_1;
@@ -14,7 +16,21 @@ typedef struct {
 	unsigned long	Reserved_4;
 	unsigned long	GPEDS[2];
 	unsigned long	Reserved_5;
-	// etc etc
+	unsigned long	GPREN[2];
+	unsigned long	Reserved_6;
+	unsigned long	GPFEN[2];
+	unsigned long	Reserved_7;
+	unsigned long	GPHEN[2];
+	unsigned long	Reserved_8;
+	unsigned long	GPLEN[2];
+	unsigned long	Reserved_9;
+	unsigned long	GPAREN[2];
+	unsigned long	Reserved_A;
+	unsigned long	GPAFEN[2];
+	unsigned long	Reserved_B;
+	unsigned long	GPPUD[1];
+	unsigned long	GPPUDCLK[2];
+	//Ignoring the reserved and test bytes
 } BCM2835_GPIO_REGS;
 
 volatile BCM2835_GPIO_REGS * const pRegs = (BCM2835_GPIO_REGS *) (0x20200000);
@@ -41,3 +57,38 @@ void SetGpio(unsigned int pinNum, unsigned int pinVal) {
 		pRegs->GPCLR[offset] = 1 << (pinNum % 32);
 	}
 }
+
+void EnableGpioDetect(unsigned int pinNum, enum DETECT_TYPE type)
+{
+	unsigned long mask=(1<<pinNum);
+	
+	switch(type) {
+	case DETECT_RISING:
+		*((unsigned long*)(pRegs->GPREN))|=mask;
+		break;
+	case DETECT_FALLING:
+		*((unsigned long*)(pRegs->GPFEN))|=mask;
+		break;
+	case DETECT_NONE:
+	default:
+		break;
+	}
+}
+
+void DisableGpioDetect(unsigned int pinNum, enum DETECT_TYPE type)
+{
+	unsigned long mask=~(1<<pinNum);
+	
+	switch(type) {
+	case DETECT_RISING:
+		*((unsigned long*)(pRegs->GPREN))&=mask;
+		break;
+	case DETECT_FALLING:
+		*((unsigned long*)(pRegs->GPFEN))&=mask;
+		break;
+	case DETECT_NONE:
+	default:
+		break;
+	}
+}
+
